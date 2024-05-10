451b1827bd82fbea0545fb8f0a4432b9b98622f6
Filipi Rafael <filipirafael.123@gmail.com>
2024-05-10 19:30:38 -0300

    reordering landing page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
               href="/auth/login"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
-              Logar<span aria-hidden="true">&rarr;</span>
+              Logar{" "}<span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
@@ -198,6 +198,48 @@ export default function Home() {
           />
         </div>
       </div>
+      <div className="overflow-hidden bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div className="lg:pr-8 lg:pt-4">
+              <div className="lg:max-w-lg">
+                <h2 className="text-base font-semibold leading-7 text-orange-600">
+                  Escolha os produtos do seu kit
+                </h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                  Tudo sob medida para você
+                </p>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Após a seleção iremos preparar os seus mockups e enviar o seu
+                  orçamento. Depois é apenas aproveitar os seguintes benefícios.{" "}
+                </p>
+                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                  {features.map((feature) => (
+                    <div key={feature.name} className="relative pl-9">
+                      <dt className="inline font-semibold text-gray-900">
+                        <feature.icon
+                          className="absolute left-1 top-1 h-5 w-5 text-orange-600"
+                          aria-hidden="true"
+                        />
+                        {feature.name}
+                      </dt>{" "}
+                      <br />
+                      <dd className="inline">{feature.description}</dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+            <Image
+              src="/appscreenshot.png"
+              alt="Product screenshot"
+              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+              width={2432}
+              height={1442}
+            />
+          </div>
+        </div>
+      </div>
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
@@ -238,48 +280,6 @@ export default function Home() {
               alt="Statamic"
               width={158}
               height={48}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="overflow-hidden bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div className="lg:pr-8 lg:pt-4">
-              <div className="lg:max-w-lg">
-                <h2 className="text-base font-semibold leading-7 text-orange-600">
-                  Escolha os produtos do seu kit
-                </h2>
-                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  Tudo sob medida para você
-                </p>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Após a seleção iremos preparar os seus mockups e enviar o seu
-                  orçamento. Depois é apenas aproveitar os seguintes benefícios.{" "}
-                </p>
-                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                  {features.map((feature) => (
-                    <div key={feature.name} className="relative pl-9">
-                      <dt className="inline font-semibold text-gray-900">
-                        <feature.icon
-                          className="absolute left-1 top-1 h-5 w-5 text-orange-600"
-                          aria-hidden="true"
-                        />
-                        {feature.name}
-                      </dt>{" "}
-                      <br />
-                      <dd className="inline">{feature.description}</dd>
-                    </div>
-                  ))}
-                </dl>
-              </div>
-            </div>
-            <Image
-              src="/appscreenshot.png"
-              alt="Product screenshot"
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-              width={2432}
-              height={1442}
             />
           </div>
         </div>
